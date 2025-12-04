1894febc3a3157091f40984fa4a4fe7c40b8fe6c
Aras Lamenni <manismi2me@gmail.com>
2025-12-04 08:05:30 +0700

    ini belajar

---
+++ food.py
@@ -0,0 +1,15 @@
+from turtle import Turtle
+import random
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.shape("circle")
+        self.shapesize(stretch_wid= 1, stretch_len=1)
+        self.color("yellow")
+        self.speed("fastest")
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y)
+
+
---
+++ inheritance class.py
@@ -0,0 +1,14 @@
+class Animal:
+    def __init__(self):
+        self.num_eyes = 2
+
+    def breathe(self):
+        print("inhale, exhale")
+
+class Fish:
+    def swim(self):
+        print("moving in water")
+
+nemo = Fish()
+
+
