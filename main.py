d33c940a25349358874b24715115c99e707f909c
Aras Lamenni <manismi2me@gmail.com>
2025-12-08 08:25:03 +0700

    add scoreboard and collisions with the wall

--- main.py
+++ main.py
@@ -28,15 +28,18 @@ game_is_on = True
 
 while game_is_on:
     screen.update()
-    time.sleep(0.15)
+    time.sleep(0.3)
     snake.move()
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
-    if snake.head.xcor()>280 or snake.head.xcor() < -280 or snake.head.ycor()>280 or snake.head.ycor() < -280:
+
+
+    if snake.head.xcor()>280 or snake.head.xcor() < -300 or snake.head.ycor()>280 or snake.head.ycor() < -280:
         game_is_on = False
         scoreboard.game_over()
-
+    # else snake.head()
 
 
 
